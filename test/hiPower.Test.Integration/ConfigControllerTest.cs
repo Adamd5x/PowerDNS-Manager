b42f8199c770022585c70820e1364501adbd1d4e
Adam Dębowski <adam.debowski.vs@utlook.com>
2024-11-25 10:02:55 +0100

    Added config controlle integration test and testing startup

---
+++ test/hiPower.Test.Integration/ConfigControllerTest.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit.Abstractions;
+
+namespace hiPower.Test.Integration
+{
+    public class ConfigControllerTest: IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> factory;
+        private readonly ITestOutputHelper outputHelper;
+
+        public ConfigControllerTest(WebApplicationFactory<Program> factory,
+                                    ITestOutputHelper helper)
+        {
+            this.factory = factory;
+            this.outputHelper = helper;
+        }
+
+        [Fact]
+        public async Task  GetConfig_RetunsOkResult()
+        {
+            // Arrange
+            string testEndpoint = "api/config";
+            HttpClient client = factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(testEndpoint);
+            outputHelper.WriteLine ($"Testing endpoint url: {testEndpoint}");
+
+            // Assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        }
+    }
+}
---
+++ test/hiPower.Test.Integration/StartUpTest.cs
@@ -0,0 +1,55 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit.Abstractions;
+
+namespace hiPower.Test.Integration
+{
+    public class StartUpTest: IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> factory;
+        private readonly ITestOutputHelper outputHelper;
+
+        private readonly List<Type> controllerList = typeof(Program).Assembly
+                                                                    .GetTypes()
+                                                                    .Where(x => x.IsSubclassOf(typeof(ControllerBase)))
+                                                                    .ToList();
+
+        public StartUpTest(WebApplicationFactory<Program> factory,
+                           ITestOutputHelper outputHelper)
+        {
+            this.factory = factory;
+            this.outputHelper = outputHelper;
+
+            this.factory = factory.WithWebHostBuilder (builder =>
+            {
+                builder.ConfigureServices (services =>
+                {
+                    controllerList.ForEach(controller => services.AddScoped(controller));
+                });
+            });
+        }
+
+
+        [Fact]
+        public void ConfigureServices_ForControllers_RegisterAllDependences ()
+        {
+            // Arrange
+            var scopeFactory = factory.Services.GetService<IServiceScopeFactory> ();
+            using var scope = scopeFactory?.CreateScope();
+
+            // Act
+
+            // Assert
+            controllerList.ForEach (item =>
+            {
+                if (scope is not null)
+                {
+                    var controller = scope.ServiceProvider.GetService(item);
+                    controller.Should ().NotBeNull ();
+                    outputHelper.WriteLine ($"Controller {controller?.GetType ().Name} created.");
+                }
+            });
+        }
+    }
+}
